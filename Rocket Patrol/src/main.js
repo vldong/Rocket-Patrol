5c8fd10d9802a69861d8a7d8173f1ad06a095a24
Victor Dong <vldong@ucsc.edu>
2020-04-08 15:47:17 -0700

    scenes & assets implemented

--- Rocket Patrol/src/main.js
+++ Rocket Patrol/src/main.js
@@ -2,6 +2,7 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    scene: [ Menu, Play ],
     //configs allow physics configurations, 
     //web browser scaling, size, etc.
 };
