06f4258fbe65a7aca4d1d456c38b5c7fa9ea74a0
Victor Dong <vldong@ucsc.edu>
2020-04-08 14:25:52 -0700

    initial commit: file structure

---
+++ Rocket Patrol/src/main.js
@@ -0,0 +1,9 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    //configs allow physics configurations, 
+    //web browser scaling, size, etc.
+};
+
+let game = new Phaser.Game(config);
